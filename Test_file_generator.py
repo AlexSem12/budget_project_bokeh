757c06e92b7c5d22b9a6fb78ecbe1919ee4b5692
Alexey <alexsem12@yahoo.com>
2020-05-29 22:23:37 -0400

    test2

---
+++ Test_file_generator.py
@@ -0,0 +1,15 @@
+# Parameters
+year_start = 2010
+year_end = 2020
+start_income_amount = 40000
+budget_type = 'Increasing'
+growth_rate = 0.2
+expense_proportion = 0.8
+expense_categories = ['Food', 'Rent', 'Entertainment', 'Home Improvements', 'Car', 'Internet', 'Cell Phone', 'Health',
+                      'Insurance']
+income_categories = ['Salary', 'Cash Backs', 'Miscellaneous']
+cost_centers = ['husband', 'wife']
+
+print('Budget start year: ' + str(year_start))
+print('Budget end year: ' + str(year_end))
+print('Starting income: ' + str(start_income_amount))
